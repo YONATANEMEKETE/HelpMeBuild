8e20968cfe6793b4c13478ff4a80691001309c62
YONATANE MEKETE <yonatanemekete33@gmail.com>
2024-12-06 09:12:25 -0800

    setup color and font

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
 export default function Home() {
-  return <div>Hello World</div>;
+  return (
+    <div className="grid place-content-center h-screen font-semibold text-mytext bg-mybg text-center">
+      <p className="text-5xl font-logo">HelpMeBuild</p>
+      <p className="text-base text-mytextlight font-body">
+        the body font goes here
+      </p>
+    </div>
+  );
 }
