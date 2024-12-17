c15eda5dd03fe5f7a21e09c708c7ea81ec997921
YONATANE MEKETE <yonatanemekete33@gmail.com>
2024-12-16 18:50:21 -0800

    add observabilities

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import localFont from 'next/font/local';
 import { Mona_Sans, Genos } from 'next/font/google';
 import './globals.css';
 import { Toaster } from '@/components/ui/sonner';
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const geistSans = localFont({
   src: './fonts/GeistVF.woff',
@@ -73,6 +75,8 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} ${monaSans.variable} ${genos.variable} antialiased`}
       >
         {children}
+        <Analytics />
+        <SpeedInsights />
         <Toaster position="top-right" richColors closeButton />
       </body>
     </html>
