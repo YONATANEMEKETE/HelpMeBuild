5bf287eb4276d761930ab88e70b4a365edb3fc57
YONATANE MEKETE <yonatanemekete33@gmail.com>
2024-12-16 17:26:32 -0800

    add lenis smooth scroll

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import DotPattern from '@/components/DotPatterns';
 import Features from '@/components/landingPage/Features';
 import Footer from '@/components/landingPage/Footer';
 import Hero from '@/components/landingPage/Hero';
@@ -6,22 +5,25 @@ import { Button } from '@/components/ui/button';
 import GridPattern from '@/components/ui/grid-pattern';
 import { cn } from '@/lib/utils';
 import Link from 'next/link';
+import { ReactLenis } from '../lib/lenis';
 
 export default function Home() {
   return (
     <div className="relative overflow-clip pb-16">
-      <GridPattern
-        width={30}
-        height={30}
-        x={-1}
-        y={-1}
-        strokeDasharray={'4 2'}
-        className={cn(
-          '[mask-image:radial-gradient(700px_circle_at_center,white,transparent)] max-h-screen'
-        )}
-      />
-      <Hero />
-      <Footer />
+      <ReactLenis root>
+        <GridPattern
+          width={30}
+          height={30}
+          x={-1}
+          y={-1}
+          strokeDasharray={'4 2'}
+          className={cn(
+            '[mask-image:radial-gradient(700px_circle_at_center,white,transparent)] max-h-screen'
+          )}
+        />
+        <Hero />
+        <Footer />
+      </ReactLenis>
       {/* rebuild it again. may be */}
       {/* <Features />  */}
     </div>
